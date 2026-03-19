93c9ca6a18907300cda2c7d7df60894ab9722289
lpwp01 <plavkush773@gmail.com>
2026-03-19 16:35:44 +0530

    update2 py file

--- test.py
+++ test.py
@@ -3,6 +3,6 @@
 def main():
     print("Hello, World!")
     print("This is a simple Python test file.")
-
+    print("Python script is running successfully.")
 if __name__ == "__main__":
     main()
